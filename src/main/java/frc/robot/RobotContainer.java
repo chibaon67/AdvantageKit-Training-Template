067cecbb0ad5c764e4243eebb50495fc75cf295c
Chibao Nguyen <chibaon678@gmail.com>
2026-09-10 22:51:54 -0500

    FIx error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,8 +51,5 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+  
 }
